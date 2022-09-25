1044c56b572ed71f30a024132152de256fb74160
Solomon <solomon@sigfried.org>
2022-09-25 16:24:56 -0400

    added buttons

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Box from '@mui/material/Box';
+const buttons = [
+  <Button key="one">One</Button>,
+  <Button key="two">Two</Button>,
+  <Button key="three">Three</Button>,
+];
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <p>
+      <p>
           Welcome To ClimeLine.
         </p>
         <p>
           An Interactive interface where you can see the effects a various climate bills
         </p>
+        </header>
+      <body className="App-body">
+      <Box
+      sx={{
+        display: 'flex',
+        '& > *': {
+          m: 1,
+        },
+      }}
+    >
+      <ButtonGroup
+        orientation="vertical"
+        aria-label="vertical outlined button group"
+      >
+        {buttons}
+      </ButtonGroup>
+    </Box>
+        
+        
         <img src="https://www.climate.gov/sites/default/files/styles/embedded_gif/public/2022-05/Screen%20Shot%202022-05-13%20at%204.05.00%20PM.png?itok=aw1T98fq" className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="https://reacjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     
+    
+     </body></div>
   );
 }
+
+
 
 export default App;
